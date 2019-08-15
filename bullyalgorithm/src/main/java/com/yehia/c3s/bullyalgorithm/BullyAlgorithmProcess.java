7235754ff4d8b635adddcbfdc89414f28c2830d5
khawanky <khawanky@gmail.com>
2019-08-15 17:55:52 +0100

    Test

--- bullyalgorithm/src/main/java/com/yehia/c3s/bullyalgorithm/BullyAlgorithmProcess.java
+++ bullyalgorithm/src/main/java/com/yehia/c3s/bullyalgorithm/BullyAlgorithmProcess.java
@@ -14,8 +14,24 @@ public class BullyAlgorithmProcess {
 	
 	public static void initProcess(int processId) {
 		try {
-			MessageCommunication.sendMessage("SharedMemoryFile", processId+"_"+"ELECTION");
-			MessageCommunication.receiveMessage("SharedMemoryFile");
+			if(processId == 1) 
+				MessageCommunication.sendMessage("SharedMemoryFile", processId+"_"+"ELECTION");
+			
+			if(processId == 2) {
+				String messageReceived = MessageCommunication.receiveMessage("SharedMemoryFile");
+				String[] content = messageReceived.split("_");
+				
+				if(content[1].equals("ELECTION")) {
+					
+				} else if(content[1].equals("ALIVE")) {
+					// Stop sending and wait for Victory message TODO
+					
+				} else if(content[1].equals("VICTORY")) {
+					// Consider the sender the coordinator TODO
+					
+				}
+				
+			}
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
--- bullyalgorithm/src/main/java/com/yehia/c3s/bullyalgorithm/MessageCommunication.java
+++ bullyalgorithm/src/main/java/com/yehia/c3s/bullyalgorithm/MessageCommunication.java
@@ -11,21 +11,23 @@ public class MessageCommunication {
 	public static final char messageDelimiter = '\0';
 	
 	
-	public static void receiveMessage(String sharedFileName) throws Throwable {
+	public static String receiveMessage(String sharedFileName) throws Throwable {
 		File f = new File(sharedFileName);
         FileChannel channel = FileChannel.open( f.toPath(), StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.CREATE );
 
         MappedByteBuffer b = channel.map( MapMode.READ_WRITE, 0, 4096 );
         CharBuffer charBuf = b.asCharBuffer();
 
-        // Prints 'Hello server'
+        // Returns the received message
+        StringBuffer messageReceived = new StringBuffer();
+        
         char c;
         while((c=charBuf.get()) != 0) {
-            System.out.print(c);
+        	messageReceived.append(c);
         }
-        System.out.println();
-
         charBuf.put(0, messageDelimiter);
+        
+        return messageReceived.toString();
 	}
 	
 	public static void sendMessage(String sharedFileName, String message) throws Throwable {
@@ -40,7 +42,8 @@ public class MessageCommunication {
         charBuf.put(string);
 
         System.out.println("Waiting for client.");
-        while( charBuf.get(0) != messageDelimiter);
+        while(charBuf.get(0) != messageDelimiter);
         System.out.println("Finished waiting.");
 	}
+	
 }
