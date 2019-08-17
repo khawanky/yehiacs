f5305a8d4f4c5df45639e3dfb0106f09bc13ae31
khawanky <khawanky@gmail.com>
2019-08-17 15:36:15 +0100

    adding random numbers Utils

--- bullyalgorithm/src/main/java/com/yehia/c3s/bullyalgorithm/utils/Utils.java
+++ bullyalgorithm/src/main/java/com/yehia/c3s/bullyalgorithm/utils/Utils.java
@@ -3,11 +3,31 @@ package com.yehia.c3s.bullyalgorithm.utils;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 public class Utils {
 	public static String getFormattedTime (long time) {
 		DateFormat simple = new SimpleDateFormat("dd/MM/yyyy | HH:mm:ss:SSS"); 
         Date result = new Date(time); 
         return simple.format(result);
+	}
+	
+	public static int[] generateRandomNumersArray(int arraySize, int numbersRange) {
+		int[] randomNumbers = new int[arraySize];
+		Random randomNumberGenerator = new Random();
+		for (int i : randomNumbers) {
+			randomNumbers[i] = randomNumberGenerator.nextInt(numbersRange);
+		}
+		return randomNumbers;
+	}
+	
+	public static int getMinimumNumber(int[] numbers) {
+		int minValue = numbers[0];
+		  for(int i=1;i<numbers.length;i++){
+		    if(numbers[i] < minValue){
+			  minValue = numbers[i];
+			}
+		  }
+		  return minValue;
 	}
 }
